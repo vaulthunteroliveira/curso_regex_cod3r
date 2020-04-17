f888b21e018beb4f2e0989e13a053c2c9f0c8ec3
alexmarques <alex.marques@salvador.ba.gov.br>
2020-04-17 12:22:35 -0300

    primeiro commit

---
+++ files.js
@@ -0,0 +1,17 @@
+const filesystem = require('fs')
+const dirname = `${__dirname}`
+
+const read = nomeArquivo => {
+    return filesystem.readFileSync(`${dirname}/originais/${nomeArquivo}`, {encoding: 'utf8'})
+} 
+
+const write = (nomeArquivo, conteudo) => {
+    const alteradosdir = `${dirname}/alterados`
+    if(!filesystem.existsSync(alteradosdir)){
+        filesystem.mkdirSync(alteradosdir)
+    }
+
+    filesystem.writeFileSync(`${dirname}/${nomeArquivo}`, conteudo, {encoding: 'utf8'})
+}
+
+module.exports = {read, write}
